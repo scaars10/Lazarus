de6cb1bbbcfbce831a51a238c74328e0b29fe9d6
scaars13 <scaars10@gmail.com>
2018-04-10 15:56:52 +0530

    Did some reorganization of scripts.

--- Virtual_Assistant.py
+++ Virtual_Assistant.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0,'src')
 import get_news
 import text_speech
 import wiki_src
@@ -5,5 +7,5 @@ import file_search
 import img_srch
 import speech_text
 import dev_handle
-
+import test
 text_speech.say("Hello, How may I help You?")
